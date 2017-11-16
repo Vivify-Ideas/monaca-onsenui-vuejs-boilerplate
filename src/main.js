57136d41c30cc506a645d8611b2856931a6c3982
Milos Janjic <milosh012@gmail.com>
2017-11-16 22:15:39 +0100

    Added vivify custom css theme.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue';
 import VueOnsen from 'vue-onsenui';
 
 import 'onsenui';
+
 // Onsen UI Styling and Icons
-require('onsenui/css-components-src/src/onsen-css-components.css');
+require('./styles/vivify-onsen-css-components.css');
 require('onsenui/css/onsenui.css');
 
 import axios from 'axios';
